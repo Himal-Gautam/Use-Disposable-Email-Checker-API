083f7cd811e32ca38ef4cda2b6d7d776966c1708
Himal-Gautam <80145259+Himal-Gautam@users.noreply.github.com>
2022-02-02 20:52:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 document.addEventListener("DOMContentLoaded", function (){
+    //selecting the card body
     const card = document.querySelector(".card-body")
 
-    
-     //url to fetch
+    //url to fetch
     const URL = "https://open.kickbox.com/v1/disposable/"
     
-    //displays facts on page
-    // function display(ind){
-    //     facts_container.innerHTML = 
-    //     `
-    //     <div class="card">
-    //     <div class="card-header">
-    //         FACT ${ind}
-    //     </div>
-    //     <div class="card-body">
-    //         <blockquote class="blockquote mb-0">
-    //         <p>${allFacts[ind]}</p>
-    //         </blockquote>
-    //     </div>
-    //     </div>
-    //      `
-    // }
-
     document.querySelector('.my-form').addEventListener("submit", function(e) {
         e.preventDefault();
       
-
+    
+    //getting the mail from input box
     const mail = document.querySelector(".input_mail").value
 
         console.log(mail);
 
+        //fetching data
         fetch(URL+mail)
         .then((response) => {
             response.json()
             .then((data) => {
                 console.log(data.disposable);
                 
+                //if not disposable then alert => This is a valid email !
                 if(!(data.disposable)){
                     card.innerHTML += `
                     <div class="alert alert-success" role="alert">
@@ -43,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function (){
                     </div>
                     `
                 }
+                //if diposable then alert => This is a disposable email !
                 else{
                     card.innerHTML += `
                     <div class="alert alert-danger" role="alert">
